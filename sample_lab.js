27bc533d305f83fe696e34efd999f95bf51b93bf
동준 김 <korfriend@gmail.com>
2022-04-13 10:26:54 +0900

    class lab codes

--- sample_lab.js
+++ sample_lab.js
@@ -2,13 +2,35 @@ const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
 
-function onResults(results) {
+import * as THREE from './node_modules/three/build/three.module.js';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+
+const renderer = new THREE.WebGLRenderer();
+const render_w = 640;
+const render_h = 480;
+renderer.setSize( render_w, render_h );
+renderer.setViewport(0, 0, render_w, render_h);
+document.body.appendChild( renderer.domElement );
+
+const camera_ar = new THREE.PerspectiveCamera( 45, render_w/render_h, 1, 500 );
+camera_ar.position.set( 0, 0, 100 );
+camera_ar.up.set(0, 1, 0);
+camera_ar.lookAt( 0, 0, 0 );
+
+const scene = new THREE.Scene();
+
+const texture_bg = new THREE.VideoTexture( videoElement );
+scene.background = texture_bg; 
+
+
+function onResults2(results) {
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
   canvasCtx.drawImage(
       results.image, 0, 0, canvasElement.width, canvasElement.height);
   if (results.multiFaceLandmarks) {
     for (const landmarks of results.multiFaceLandmarks) {
+      // landmarks is NormalizedLandmark[]
       drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION,
                      {color: '#C0C0C070', lineWidth: 1});
       drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {color: '#FF3030'});
@@ -21,11 +43,12 @@ function onResults(results) {
       drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
     }
   }
+  renderer.render( scene, camera_ar );
   canvasCtx.restore();
 }
 
 const faceMesh = new FaceMesh({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+  return `./node_modules/@mediapipe/face_mesh/${file}`;
 }});
 faceMesh.setOptions({
   maxNumFaces: 1,
@@ -33,13 +56,22 @@ faceMesh.setOptions({
   minDetectionConfidence: 0.5,
   minTrackingConfidence: 0.5
 });
-faceMesh.onResults(onResults);
+faceMesh.onResults(onResults2);
 
-const camera = new Camera(videoElement, {
-  onFrame: async () => {
-    await faceMesh.send({image: videoElement});
-  },
-  width: 1280,
-  height: 720
-});
-camera.start();
+//const camera = new Camera(videoElement, {
+//  onFrame: async () => {
+//    await faceMesh.send({image: videoElement});
+//  },
+//  width: 1280,
+//  height: 720
+//});
+//camera.start();
+
+videoElement.play();
+
+async function detectionFrame() {
+  await faceMesh.send({image: videoElement});
+  videoElement.requestVideoFrameCallback(detectionFrame);
+}
+
+detectionFrame();
