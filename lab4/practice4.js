a9db8416f4526ef7a3a03773a9fa8fdf26795b50
korfriend <korfriend@gmail.com>
2022-05-18 02:41:46 +0900

    pose rigging test

--- lab4/practice4.js
+++ lab4/practice4.js
@@ -8,6 +8,7 @@ import { OrbitControls } from '../node_modules/three/examples/jsm/controls/Orbit
 import { TRIANGULATION } from '../triangulation.js';
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 import { CalculateJointAngles } from './calculate_joint_angles.js';
+import '../node_modules/@mediapipe/holistic/holistic.js';
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 const render_w = 640;
@@ -74,14 +75,15 @@ loader.load( '../models/gltf/Xbot.glb', function ( gltf ) {
           axis_helper.material.depthTest = false;
           bones.push(object);
           //console.log(object);
-          object.add(axis_helper);
+          //object.add(axis_helper);
         }
     } );
 
     bones.forEach(function(bone){
         console.log(bone.name);
-        bone.matrixAutoUpdate = false;
-        bone.matrix = new THREE.Matrix4().makeTranslation(bone.position.x, bone.position.y, bone.position.z);
+        //bone.matrixAutoUpdate = false;
+        //bone.setRotationFromAxisAngle ( new THREE.Vector3(1, 0, 0), 3.14/2 ) ;
+        //bone.matrix = new THREE.Matrix4().makeTranslation(bone.position.x, bone.position.y, bone.position.z);
     });
 
     skeleton = new THREE.Skeleton(bones);
@@ -109,6 +111,40 @@ function ProjScale(p_ms, cam_pos, src_d, dst_d) {
   let vec_cam2p = new THREE.Vector3().subVectors(p_ms, cam_pos);
   return new THREE.Vector3().addVectors(cam_pos, vec_cam2p.multiplyScalar(dst_d/src_d));
 }
+
+const rigRotation = (
+  name,
+  rotation = { x: 0, y: 0, z: 0 },
+  dampener = 1,
+  lerpAmount = 0.3
+  ) => {
+  const Part = skeleton.getBoneByName(name);
+  if (!Part) {return}
+  
+  let euler = new THREE.Euler(
+    -rotation.x * dampener,
+    rotation.y * dampener,
+    -rotation.z * dampener
+  );
+  let quaternion = new THREE.Quaternion().setFromEuler(euler);
+  Part.quaternion.slerp(quaternion, lerpAmount); // interpolate
+};
+
+const rigPosition = (
+  name,
+  position = { x: 0, y: 0, z: 0 },
+  dampener = 1,
+  lerpAmount = 0.3
+  ) => {
+  const Part = skeleton.getBoneByName(name);
+  if (!Part) {return}
+  let vector = new THREE.Vector3(
+    position.x * dampener,
+    position.y * dampener,
+    position.z * dampener
+  );
+  Part.position.lerp(vector, lerpAmount); // interpolate
+};
 
 function onResults2(results) {
   if (!results.poseLandmarks) {
@@ -150,16 +186,62 @@ function onResults2(results) {
   //    imageSize: { height: 462, width: 820 },
   //    enableLegs: true,
   //});
-  let poselm3d = results.poseLandmarks;
-  let poseRig = Kalidokit.Pose.solve(poselm3d,results.poseLandmarks,{runtime:'mediapipe',imageSize:{width:820, height:462}})
+
+  //console.log("GG == ", results.poseLandmarks[19]);
+  // Pose 3D Landmarks are with respect to Hip distance in meters
+  // let poselm3d = [];
+  results.poseLandmarks.forEach(function(landmark){
+    //landmark.x = -(landmark.x - 0.5) * 2.0;
+    //landmark.y = -(landmark.y - 0.5) * 2.0;
+    //landmark.z = -landmark.z;
+  });
+  results.ea.forEach(function(landmark){
+    //landmark.x = -landmark.x;
+    //landmark.y = -landmark.y;
+    //landmark.z = -landmark.z;
+  });
+  //console.log("GL == ", results.poseLandmarks[0].y);
+
+//  let poselm3d = results.poseLandmarks;
+  let poseRig = Kalidokit.Pose.solve(results.ea,results.poseLandmarks,{runtime:'mediapipe',imageSize:{width:820, height:462}});
+  rigRotation("Hips", poseRig.Hips.rotation, 1.0);
+  rigPosition(
+    "mixamorigHips",
+    {
+      x: -poseRig.Hips.position.x * 100, // Reverse direction
+      y: poseRig.Hips.position.y * 100 + 100, // Add a bit of height
+      z: -poseRig.Hips.position.z * 100 // Reverse direction
+    },
+    1,
+    0.9
+  );
+  
+  rigRotation("mixamorigSpine", poseRig.Spine, 1, .8);
+  rigRotation("mixamorigSpine1", poseRig.Spine, 1, .8);
+  rigRotation("mixamorigSpine2", poseRig.Spine, 1, .8);
+
+  rigRotation("mixamorigLeftUpLeg", poseRig.LeftUpperLeg, 1, .8);
+  rigRotation("mixamorigLeftLeg", poseRig.LeftLowerLeg, 1, .8);
+  rigRotation("mixamorigRightUpLeg", poseRig.RightUpperLeg, 1, .8);
+  rigRotation("mixamorigRightLeg", poseRig.RightLowerLeg, 1, .8);
+
+  rigRotation("mixamorigRightArm", poseRig.RightUpperArm, 1, .8);
+  rigRotation("mixamorigRightForeArm", poseRig.RightLowerArm, 1, .8);
+  rigRotation("mixamorigLeftArm", poseRig.LeftUpperArm, 1, .8);
+  rigRotation("mixamorigLeftForeArm", poseRig.LeftLowerArm, 1, .8);
+
   //console.log(poseRig.LeftUpperLeg);
   //console.log(poseRig.LeftLowerLeg);
   //console.log(poseRig);
-  let hips = skeleton.getBoneByName("mixamorigHips");
-  let mixamor_leftUpLeg = skeleton.getBoneByName("mixamorigLeftUpLeg");
-  let matT_leftUpLeg = new THREE.Matrix4().makeTranslation(mixamor_leftUpLeg.position.x, mixamor_leftUpLeg.position.y, mixamor_leftUpLeg.position.z);
-  let mixamor_leftLeg = skeleton.getBoneByName("mixamorigLeftLeg");
-  let matT_leftLeg = new THREE.Matrix4().makeTranslation(mixamor_leftLeg.position.x, mixamor_leftLeg.position.y, mixamor_leftLeg.position.z);
+  //let hips = skeleton.getBoneByName("mixamorigHips");
+  //hips.position.set(-poseRig.Hips.position.x, poseRig.Hips.position.y + 10, -poseRig.Hips.position.z);
+  //let mixamor_leftUpLeg = skeleton.getBoneByName("mixamorigLeftUpLeg");
+  //let matT_leftUpLeg = new THREE.Matrix4().makeTranslation(mixamor_leftUpLeg.position.x, mixamor_leftUpLeg.position.y, mixamor_leftUpLeg.position.z);
+  //console.log("1 ::: ", mixamor_leftUpLeg.position.x, mixamor_leftUpLeg.position.y, mixamor_leftUpLeg.position.z);
+  //let mixamor_leftLeg = skeleton.getBoneByName("mixamorigLeftLeg");
+  //let matT_leftLeg = new THREE.Matrix4().makeTranslation(mixamor_leftLeg.position.x, mixamor_leftLeg.position.y, mixamor_leftLeg.position.z);
+  //console.log(mixamor_leftLeg);
+  //console.log("2 ::: ", mixamor_leftLeg.position.x, mixamor_leftLeg.position.y, mixamor_leftLeg.position.z);
   // let mixamor_rightUpLeg = skeleton.getBoneByName("mixamorigRightUpLeg");
   // let matT_rightUpLeg = new THREE.Matrix4().makeTranslation(mixamor_rightUpLeg.position.x, mixamor_rightUpLeg.position.y, mixamor_rightUpLeg.position.z);
   // let mixamor_rightLeg = skeleton.getBoneByName("mixamorigRightLeg");
@@ -189,10 +271,12 @@ function onResults2(results) {
   //let mat_rot_0 = new THREE.Matrix4().multiplyMatrices(new THREE.Matrix4().multiplyMatrices(hips_roll, hips_pitch), hips_yaw);
 
   //hips.matrix.multiplyMatrices(mat_tln_0, mat_rot_0);
-  mixamor_leftUpLeg.matrix.multiplyMatrices(sv_matR_leftupleg, matT_leftUpLeg);
-  mixamor_leftLeg.matrix.multiplyMatrices(sv_matR_leftleg, matT_leftLeg);
+  //mixamor_leftUpLeg.matrix = matT_leftUpLeg;//.multiplyMatrices(sv_matR_leftupleg, matT_leftUpLeg);
+  //mixamor_leftLeg.matrix = matT_leftLeg;//.multiplyMatrices(sv_matR_leftleg, matT_leftLeg);
   // mixamor_rightUpLeg.matrix.multiplyMatrices(sv_matR_rightupleg, matT_rightUpLeg);
   // mixamor_rightLeg.matrix.multiplyMatrices(sv_matR_rightleg, matT_rightLeg);
+  //mixamor_leftUpLeg.setRotationFromEuler(sv_euler_leftupleg);
+  //mixamor_leftLeg.setRotationFromEuler(sv_euler_leftleg);
     
   //console.log(bn_test.matrix);
   //bn_test.matrix = new THREE.Matrix4();
@@ -202,23 +286,29 @@ function onResults2(results) {
   canvasCtx.restore();
 }
 
-const pose = new Pose({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+//const pose = new Pose({locateFile: (file) => {
+//  return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+//}});
+let holistic = new Holistic({locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic@0.4.1633559476/${file}`;
 }});
-pose.setOptions({
-  modelComplexity: 1,
-  smoothLandmarks: true,
-  enableSegmentation: true,
-  smoothSegmentation: true,
-  minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5
-});
-pose.onResults(onResults2);
+
+//pose.setOptions({
+//  modelComplexity: 1,
+//  smoothLandmarks: true,
+//  enableSegmentation: true,
+//  smoothSegmentation: true,
+//  minDetectionConfidence: 0.5,
+//  minTrackingConfidence: 0.5
+//});
+//pose.onResults(onResults2);
+holistic.onResults(onResults2);
 
 videoElement.play();
 
 async function detectionFrame() {
-  await pose.send({image: videoElement});
+  //await pose.send({image: videoElement});
+  await holistic.send({image: videoElement});
   videoElement.requestVideoFrameCallback(detectionFrame);
 }
 
