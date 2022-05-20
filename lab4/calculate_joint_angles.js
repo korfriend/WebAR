daa6cb85ce3f8f05c51fe51bd25ed0dee11e33af
korfriend <korfriend@gmail.com>
2022-05-21 01:48:11 +0900

    pose solver completed

--- lab4/calculate_joint_angles.js
+++ lab4/calculate_joint_angles.js
@@ -61,6 +61,10 @@ class ThreeMpPose {
         this.newJoints3D["hips"] = new Vector3().addVectors(center_hips, dir_spine.clone().multiplyScalar(length_spine / 9.0));
         this.newJoints3D["neck"] = new Vector3().addVectors(center_shoulders, dir_spine.clone().multiplyScalar(length_spine / 9));
         this.newJoints3D["center_shoulder"] = center_shoulders;
+
+        this.pose3dDict["hips"] = this.newJoints3D["hips"];
+        this.pose3dDict["neck"] = this.newJoints3D["neck"];
+        this.pose3dDict["center_shoulder"] = this.newJoints3D["center_shoulder"];
     }
 
     transformToWorld = function(camera, dist_from_cam) {
@@ -90,59 +94,74 @@ class ThreeMpPose {
     }
 
     rigSolverForMixamo = function (skeleton) {
-        // hip joint 
-        {
-            const hip_joint = this.newJoints3D["hips"];
-            const u = new Vector3().subVectors(this.pose3dDict["left_hip"], this.pose3dDict["right_hip"]);
-            u.normalize();
-            const v = new Vector3().subVectors(this.newJoints3D["neck"], hip_joint);
-            v.normalize();
-            const w = new Vector3().crossVectors(u, v);
-            w.normalize();
+        this.computeR_hips = function(){
+            const hip_joint = this.pose3dDict["hips"];
+            let u = new Vector3().subVectors(this.pose3dDict["left_hip"], this.pose3dDict["right_hip"]).normalize();
+            const v = new Vector3().subVectors(this.pose3dDict["neck"], hip_joint).normalize();
+            const w = new Vector3().crossVectors(u, v).normalize();
+            u = new Vector3().crossVectors(v, w).normalize();
             const R = new Matrix4().makeBasis(u, v, w); // local!!
-
-            const q = new Quaternion().setFromRotationMatrix(R);
-            const hip_root = skeleton.getBoneByName("mixamorigHips");
-
-            //const skeleton_scale = hip_root.parent.scale;
-
-            hip_root.position.set(hip_joint.x, hip_joint.y + 100, hip_joint.z);
-            hip_root.quaternion.slerp(q, 0.9); 
+            return R;
         }
+
+        const R_hips = this.computeR_hips();
+        const hip_root = skeleton.getBoneByName("mixamorigHips");
+        const hip_joint = this.pose3dDict["hips"];
+        hip_root.position.set(hip_joint.x, hip_joint.y + 100, hip_joint.z);
+        hip_root.quaternion.slerp(new Quaternion().setFromRotationMatrix(R_hips), 0.9); 
+
+        this.computeJointParentR = function(nameSkeletonJoint, nameMpJoint, nameMpJointParent, R_chain, skeleton){
+            const skeletonJoint = skeleton.getBoneByName(nameSkeletonJoint);
+            const j = skeletonJoint.position.clone();
+            const v = new Vector3().subVectors(this.pose3dDict[nameMpJoint], this.pose3dDict[nameMpJointParent]).normalize();
+            let R = this.computeR(j, v.applyMatrix4(R_chain.clone().transpose()));
+            return R;
+        }
+
+        // left arms
+        {
+            let R_chain = new Matrix4();
+            const R_arm = this.computeJointParentR("mixamorigLeftForeArm", "left_elbow", "left_shoulder", R_chain, skeleton);
+            skeleton.getBoneByName("mixamorigLeftArm").quaternion.slerp(new Quaternion().setFromRotationMatrix(R_arm), 0.9);
+
+            R_chain = R_chain.multiply(R_arm);
+            const R_forearm = this.computeJointParentR("mixamorigLeftHand", "left_wrist", "left_elbow", R_chain, skeleton);
+            skeleton.getBoneByName("mixamorigLeftForeArm").quaternion.slerp(new Quaternion().setFromRotationMatrix(R_forearm), 0.9);
+        }
+
+        // right arms
+        {
+            let R_chain = new Matrix4();
+            const R_arm = this.computeJointParentR("mixamorigRightForeArm", "right_elbow", "right_shoulder", R_chain, skeleton);
+            skeleton.getBoneByName("mixamorigRightArm").quaternion.slerp(new Quaternion().setFromRotationMatrix(R_arm), 0.9);
+
+            R_chain = R_chain.multiply(R_arm);
+            const R_forearm = this.computeJointParentR("mixamorigRightHand", "right_wrist", "right_elbow", R_chain, skeleton);
+            skeleton.getBoneByName("mixamorigRightForeArm").quaternion.slerp(new Quaternion().setFromRotationMatrix(R_forearm), 0.9);
+        }
+
         // left legs
         {
-            const left_up_leg = skeleton.getBoneByName("mixamorigLeftUpLeg");
-            let j = left_up_leg.position.clone();
-            let v = new Vector3().subVectors(this.pose3dDict["left_hip"], this.newJoints3D["hips"]).normalize();
-            let R0 = this.computeR(j, v);
-            let q = new Quaternion().setFromRotationMatrix(R0);
-            left_up_leg.quaternion.slerp(q, 1.0); 
+            let R_chain = R_hips.clone();
+            const R_upleg = this.computeJointParentR("mixamorigLeftLeg", "left_knee", "left_hip", R_chain, skeleton);
+            skeleton.getBoneByName("mixamorigLeftUpLeg").quaternion.slerp(new Quaternion().setFromRotationMatrix(R_upleg), 0.9);
 
-            const left_leg = skeleton.getBoneByName("mixamorigLeftLeg");
-            j = left_leg.position.clone();
-            v = new Vector3().subVectors(this.pose3dDict["left_knee"], this.pose3dDict["left_hip"]).normalize();
-            let R1 = this.computeR(j, v.applyMatrix4(R0.transpose()));
-            q = new Quaternion().setFromRotationMatrix(R1);
-            left_leg.quaternion.slerp(q, 0.9); 
+            R_chain = R_chain.multiply(R_upleg);
+            const R_leg = this.computeJointParentR("mixamorigLeftFoot", "left_ankle", "left_knee", R_chain, skeleton);
+            skeleton.getBoneByName("mixamorigLeftLeg").quaternion.slerp(new Quaternion().setFromRotationMatrix(R_leg), 0.9);
         }
-        
-        // left legs
+
+        // right legs
         {
-            const right_up_leg = skeleton.getBoneByName("mixamorigRightUpLeg");
-            let j = right_up_leg.position.clone();
-            let v = new Vector3().subVectors(this.pose3dDict["right_hip"], this.newJoints3D["hips"]).normalize();
-            let R0 = this.computeR(j, v);
-            let q = new Quaternion().setFromRotationMatrix(R0);
-            right_up_leg.quaternion.slerp(q, 0.9); 
+            let R_chain = R_hips.clone();
+            const R_upleg = this.computeJointParentR("mixamorigRightLeg", "right_knee", "right_hip", R_chain, skeleton);
+            skeleton.getBoneByName("mixamorigRightUpLeg").quaternion.slerp(new Quaternion().setFromRotationMatrix(R_upleg), 0.9);
 
-            
-            const right_leg = skeleton.getBoneByName("mixamorigRightLeg");
-            j = right_leg.position.clone();
-            v = new Vector3().subVectors(this.pose3dDict["right_knee"], this.pose3dDict["right_hip"]).normalize();
-            let R1 = this.computeR(j, v.applyMatrix4(R0.transpose()));
-            q = new Quaternion().setFromRotationMatrix(R1);
-            right_leg.quaternion.slerp(q, 0.9); 
+            R_chain = R_chain.multiply(R_upleg);
+            const R_leg = this.computeJointParentR("mixamorigRightFoot", "right_ankle", "right_knee", R_chain, skeleton);
+            skeleton.getBoneByName("mixamorigRightLeg").quaternion.slerp(new Quaternion().setFromRotationMatrix(R_leg), 0.9);
         }
+
     }
 
     // rotate a to b
